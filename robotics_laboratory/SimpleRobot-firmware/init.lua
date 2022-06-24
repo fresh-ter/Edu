0d35844379f1d34bbbee5e8dc26a41365d8c3e21
fresh-ter <fresh-ter@yandex.com>
2022-06-24 15:48:26 +0400

    Update init.lua

--- robotics_laboratory/SimpleRobot-firmware/init.lua
+++ robotics_laboratory/SimpleRobot-firmware/init.lua
@@ -20,26 +20,49 @@ wifi.ap.setip(cfg)
 print("Hello, world!")
 
 
+-- Pin control
+
+-- 1 - left forward
+-- 2 - left back
+-- 3 - right forward
+-- 4 - right back
+
+gpio.mode(1, gpio.OUTPUT)
+gpio.mode(2, gpio.OUTPUT)
+gpio.mode(3, gpio.OUTPUT)
+gpio.mode(4, gpio.OUTPUT)
+
+
 -- Control
 
 function robot_forward( ... )
     print("Robot -> Forward")
+    gpio.write(1, gpio.HIGH)
+    gpio.write(3, gpio.HIGH)
 end
 
 function robot_stop( ... )
     print("Robot -> Stop")
+    gpio.write(1, gpio.LOW)
+    gpio.write(2, gpio.LOW)
+    gpio.write(3, gpio.LOW)
+    gpio.write(4, gpio.LOW)
 end
 
 function robot_turn_right( ... )
     print("Robot -> Right")
+    gpio.write(1, gpio.HIGH)
 end
 
 function robot_turn_left( ... )
     print("Robot -> Left")
+    gpio.write(3, gpio.HIGH)
 end
 
 function robot_back( ... )
     print("Robot -> Back")
+    gpio.write(2, gpio.HIGH)
+    gpio.write(4, gpio.HIGH)
 end
 
 -- Server
@@ -53,9 +76,11 @@ require("httpserver").createServer(80, function(req, res)
         robot_stop()
 
     elseif req.url == "/right" then
+        robot_stop()
         robot_turn_right()
 
     elseif req.url == "/left" then
+        robot_stop()
         robot_turn_left()
         
     elseif req.url == "/back" then
