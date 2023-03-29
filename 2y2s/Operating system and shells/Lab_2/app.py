4041623a2e6fdc1204798404328547b1ed5766af
Terminator <fresh-ter@yandex.com>
2023-03-29 12:33:26 +0400

    update lab_2 OS

--- 2y2s/Operating system and shells/Lab_2/app.py
+++ 2y2s/Operating system and shells/Lab_2/app.py
@@ -2,31 +2,34 @@ from tkinter import *
 from tkinter import ttk
 
 import ptrace.debugger
+import psutil
 
+
+mem_var = 0
 
 root = Tk()
 
 root.title("Memory I/O")
-root.geometry("250x200")
+root.geometry("200x230")
 
 
 def read():
     pid = int(entryPID.get())
     address = entryAddress.get()
     a = int(address, 16)
-    
+
     print(pid)
     print(type(pid))
     print(address)
     print(a)
     print(type(a))
-    
+
     p = ptrace.debugger.PtraceDebugger()
     process = p.addProcess(pid, False)
     data = process.readWord(a)
     process.detach()
 
-    labelData['text'] = data
+    labelData['text'] = "Data = <" + str(data) + ">"
 
 
 def write():
@@ -35,13 +38,13 @@ def write():
     a = int(address, 16)
 
     newData = int(entryNewData.get())
-    
+
     print(pid)
     print(type(pid))
     print(address)
     print(a)
     print(type(a))
-    
+
     p = ptrace.debugger.PtraceDebugger()
     process = p.addProcess(pid, False)
     process.writeWord(a, newData)
@@ -74,6 +77,23 @@ entryNewData.pack()
 
 buttonWrite = ttk.Button(text='Write', command=write)
 buttonWrite.pack()
+
+labelMem = ttk.Label(text='Max mem = <%>')
+labelMem.pack(expand=1, anchor=S)
+
+def update_maxMem():
+    global mem_var
+    global root
+
+    now = psutil.virtual_memory()[2]
+
+    if now > mem_var:
+        mem_var = now
+        labelMem["text"] = "Max mem = <" + str(mem_var) + "%>"
+
+    root.after(1000, update_maxMem)
+
+update_maxMem()
 
 root.mainloop()
 
